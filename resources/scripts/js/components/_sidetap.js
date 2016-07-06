ce4b0d7f5249f8a3723ada0cd0733099cceec921
vincents <vincent@overhaulmedia.com>
2016-07-06 11:31:53 -0700

    removed console log from sidetap js

--- resources/scripts/js/components/_sidetap.js
+++ resources/scripts/js/components/_sidetap.js
@@ -1,9 +1,14 @@
+/**
+ *
+ *  Sidetap
+ *  The sidetap JS handles the class toggling of the navigational menu on mobile devices.
+ *
+ */
+
 (function(){
 
   var sidetap_link = document.getElementsByClassName('side-tap')[0],
       body = document.body;
-
-      console.log(sidetap_link);
 
   sidetap_link.addEventListener('click', e => body.classList.toggle('side-tap--open'));
 
