dbd9a377be1219036e5064424da00f06a74f7705
vincents <vincent@overhaulmedia.com>
2016-07-05 23:13:55 -0700

    changed h1 for svg

--- public/parts/components/header.php
+++ public/parts/components/header.php
@@ -11,9 +11,18 @@
 
 <header class="header">
 
-  <h1 class="logo"><a href="/" class="logo__link">
-    Cut Your Veggies
-  </a></h1>
+  <a href="/" class="logo__link">
+    <svg class="logo" viewbox="0 0 340 60">
+      <text fill="#ffffff"
+            font-size="35"
+            font-family="Cutive Mono"
+            alignment-baseline="middle"
+            x="0"
+            y="32">
+        Cut Your Veggies
+      </text>
+    </svg>
+  </a>
 
   <?php include "parts/components/main-nav.php"; ?>
 
