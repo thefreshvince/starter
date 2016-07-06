6fca6460b0a3641534a50fe7bb54a6f3db68fdf3
vincents <vincent@overhaulmedia.com>
2016-07-05 22:03:08 -0700

    Added title and description

--- public/index.php
+++ public/index.php
@@ -2,12 +2,18 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Site Title</title>
+  <title>Cut the Veggies</title>
   <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
 
   <?php include('parts/components/header.php'); ?>
+
+  <div class="container">
+    <p>
+      Web development is alot like cooking... well sorta, I mean you shouldn't go kniving your monitor. Cutting the veggies was born out of my personal want to quickly develop deployable non weighted front-end files.
+    </p>
+  </div>
 
 </body>
 </html>
