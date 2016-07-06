529c639da037fd4c681e2fef3a83e729dd2073ea
vincents <vincent@overhaulmedia.com>
2016-07-05 22:04:26 -0700

    renamed main nav file

---
+++ public/parts/components/main-nav.php
@@ -0,0 +1,50 @@
+<?php
+
+  /*
+   *
+   *  NAVIGATION
+   *  This is the main navigation for the website
+   *
+   */
+
+  // Grab the navigation data
+  include_once "parts/data/navigation.php";
+
+?>
+
+<nav class="main-nav">
+  <a href="#side-tap" class="side-tap show-on-mobile"></a>
+  <ul class="main-nav__list hide-on-mobile">
+    <?php
+
+      // Loop through the navigation elements
+      foreach ($NAVIGATION as $link_title => $link_value) {
+
+        $link_url = '';
+
+        // Determine if the link has sub navigation
+        if(is_string($link_value)) {
+          $link_url = $link_value;
+        } else {
+          $link_url = $link_value['link'];
+        }
+
+        echo '<li class="main-nav__list-item"><a href="' , $link_url , '" class="main-nav__link">' , $link_title , '</a>';
+
+          // If subnav exists, loop through the items
+          if(is_array($link_value)) {
+            echo '<ul class="main-nav__list main-nav__sub">';
+              foreach ($link_value as $subnav_link_title => $subnav_link_value) {
+                if($subnav_link_title == 'link') continue;
+                echo '<li><a href="' , $subnav_link_value , '" class="main-nav__link main-nav__sub-link">' , $subnav_link_title , '</a></li>';
+              }
+            echo '</ul>';
+          }
+
+        echo '</li>';
+
+      }
+
+    ?>
+  </ul>
+</nav>
