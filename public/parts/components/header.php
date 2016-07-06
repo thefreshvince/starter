89bc02664b9eec92564c5dd3abc1920c81613189
vincents <vincent@overhaulmedia.com>
2016-07-05 22:03:26 -0700

    added logo

--- public/parts/components/header.php
+++ public/parts/components/header.php
@@ -11,6 +11,10 @@
 
 <header class="header">
 
+  <h1 class="logo"><a href="/" class="logo__link">
+    Cut Your Veggies
+  </a></h1>
+
   <?php include "parts/components/main-nav.php"; ?>
 
 </header>
