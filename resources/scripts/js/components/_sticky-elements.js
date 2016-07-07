2abf5c04005f2097d08fa415546994305d306852
vincents <vincent@overhaulmedia.com>
2016-07-06 19:06:39 -0700

    Added dropdown styles

--- resources/scripts/js/components/_sticky-elements.js
+++ resources/scripts/js/components/_sticky-elements.js
@@ -7,4 +7,22 @@
 
 (function(){
 
+  var sticky_elements = document.getElementsByClassName('sticky'),
+      Sticky = function(el){
+        this.el = el;
+        this.par = el.parentNode;
+        this.el_start = null; // determined by a bunch of things
+        this.el_end = null; // determined by a bunch of things
+        this.init();
+      };
+
+  Sticky.prototype.scroll = function () {};
+
+  Sticky.prototype.resize = function () {};
+
+  Sticky.prototype.init = function () {
+    window.addEventListener('resize', this.resize);
+    window.addEventListener('scroll', this.scroll);
+  };
+
 })();
