416e85ef3c21983a0e6a2094e2ac94442d887d34
vincents <vincent@overhaulmedia.com>
2016-07-07 10:50:41 -0700

    Added newsletter components

---
+++ public/parts/components/newsletter.php
@@ -0,0 +1,8 @@
+<p>This brief blurb should describe what they'll get from the newsletter.</p>
+<div class="newsletter">
+  <form action="#Newsletter" method="post" class="newsletter__form">
+    <input class="newsletter__input" type="email" name="email" placeholder="Enter your email" required>
+    <button class="newsletter__submit">Join</button>
+    <span class="newsletter__message"></span>
+  </form>
+</div>
