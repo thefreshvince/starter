8b614ba119221c9198fe7e2f0b807a1a24daa033
vincents <vincent@overhaulmedia.com>
2016-07-09 09:45:27 -0700

    added data for the swiper

---
+++ public/parts/data/swiper.php
@@ -0,0 +1,16 @@
+<?php
+
+  /**
+   *  Swiper
+   *  @var Array
+   */
+  $SWIPER = [
+    'This is...',
+    'The swiper...',
+    'That we use',
+    'Idangerous Swiper',
+    'Requires no jQuery',
+    'And has comprehensive docs'
+  ];
+
+?>
