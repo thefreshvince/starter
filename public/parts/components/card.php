afe565224ec3fa0988439ad030b04686f32d690c
vincents <vincent@overhaulmedia.com>
2016-07-09 11:00:48 -0700

    added conditionals throughout the card include

--- public/parts/components/card.php
+++ public/parts/components/card.php
@@ -12,37 +12,49 @@
 <div class="card__wrapper block--100 block--50-ml block--one-third-tp">
   <article class="card">
     <header class="card__header">
-      <img src="http://placehold.it/350x200/ffffff/ffffff" alt="card placeholder" class="card__img">
-      <div class="v-center">
-        <div>
-          <h2 class="card__title h2"><?= $card['title'] ?></h2>
+      <?php if(!empty($card['img'])){ ?>
+        <img src="<?= $card['img'] ?>" alt="card placeholder" class="card__img">
+      <?php } ?>
+      <?php if(!empty($card['title'])){ ?>
+        <div class="v-center">
+          <div>
+            <h2 class="card__title h2"><?= $card['title'] ?></h2>
+          </div>
         </div>
-      </div>
+      <?php } ?>
     </header>
 
     <div class="card-body block--100">
-      <time class="card--timestamp" datetime="<?= $card['date'] ?>">
-        <?= date('F j, Y', strtotime($card['date'])) ?>
-      </time>
-      <address>
-        <a class="card__author" href="#">
-          by <?= $card['author'] ?>
-        </a>
-      </address>
-      <p class="card__excerpt"><?= $card['excerpt'] ?></p>
+      <?php if(!empty($card['date'])){ ?>
+        <time class="card--timestamp" datetime="<?= $card['date'] ?>">
+          <?= date('F j, Y', strtotime($card['date'])) ?>
+        </time>
+      <?php } ?>
+      <?php if(!empty($card['author'])){ ?>
+        <address>
+          <a class="card__author" href="#">
+            by <?= $card['author'] ?>
+          </a>
+        </address>
+      <?php } ?>
+      <?php if(!empty($card['excerpt'])){ ?>
+        <p class="card__excerpt"><?= $card['excerpt'] ?></p>
+      <?php } ?>
     </div>
 
     <footer class="card__footer">
-      <ul class="card__tags ul-reset li-inline">
-        <?php
+      <?php if(!empty($card['tags'])){ ?>
+        <ul class="card__tags ul-reset li-inline">
+          <?php
 
-          foreach ($card['tags'] as $tag) {
-            echo '<li class="card__tag"><a href="#' , $tag , '">' ,
-              $tag , '</a></li>';
-          }
+            foreach ($card['tags'] as $tag) {
+              echo '<li class="card__tag"><a href="#' , $tag , '">' ,
+                $tag , '</a></li>';
+            }
 
-        ?>
-      </ul>
+          ?>
+        </ul>
+      <?php } ?>
     </footer>
   </article>
 </div>
