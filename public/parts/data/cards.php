3d00a3eb8fc41480f2aa7b2407fc9e5c4cab596b
vincents <vincent@overhaulmedia.com>
2016-07-09 11:00:28 -0700

    added img index to card arrays

--- public/parts/data/cards.php
+++ public/parts/data/cards.php
@@ -10,6 +10,7 @@
       'date'    => '1905-11-13',
       'author'  => 'Krombopulos Michael',
       'excerpt' => 'Lorem ipsum dolor sit amet, tenetur, tempore praesentium id magnam ut, illo, quas sequi voluptates architecto deserunt tempora error reiciendis facere perspiciatis! Perspiciatis.',
+      'img'     => 'http://placehold.it/350x200/ffffff/ffffff',
       'tags'    => [
         'Blitz and Chitz',
         'Science'
@@ -20,6 +21,7 @@
       'date'    => '1905-11-13',
       'author'  => 'Rick Sanchez',
       'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi distinctio natus beatae tenetur, tempore praesentium id magnam ut.',
+      'img'     => 'http://placehold.it/350x200/ffffff/ffffff',
       'tags'    => [
         'Chemistry',
         'Black Holes'
@@ -30,6 +32,7 @@
       'date'    => '1905-11-13',
       'author'  => 'Krombopulos Michael',
       'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi distinctio natus beatae tenetur, tempore praesentium id magnam ut, illo, quas sequi voluptates architecto deserunt tempora error reiciendis facere perspiciatis! Perspiciatis.',
+      'img'     => 'http://placehold.it/350x200/ffffff/ffffff',
       'tags'    => [
         'Flowers',
         'Rayguns'
